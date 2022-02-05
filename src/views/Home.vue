1a1cacb5885a2233b51aec34572cd90b5fed14b5
Patrick Winfield <patrwinfspam+github@gmail.com>
2022-02-05 10:34:52 -0700

    Add pokeapi

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="home">
     <p>{{title}}</p>
+    <button @click="onFetchPokemon">Fetch pokemon</button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
+import { PokemonClient } from 'pokenode-ts';
 
 export default defineComponent({
   name: 'Home',
@@ -13,7 +15,17 @@ export default defineComponent({
   setup() {
     const title = ref('Welcome to pokeapi explorer')
 
-    return { title }
+    const onFetchPokemon = async() => {
+      const api = new PokemonClient();
+
+      await api
+      .getPokemonByName('luxray')
+      .then((data) => console.log(data)) // will output "Luxray"
+      .catch((error) => console.error(error));
+
+    }
+
+    return { title, onFetchPokemon }
   }
 });
 </script>
