a732ba3d1e1ae6bcb1df1e6a87ad67a93389f9c0
Patrick Winfield <patrwinfspam+github@gmail.com>
2022-02-05 11:02:05 -0700

    fetch pokemon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,45 @@
 <template>
   <div class="home">
     <p>{{title}}</p>
-    <button @click="onFetchPokemon">Fetch pokemon</button>
+    <input type="text" placeholder="Enter name of pokeman to search" v-model="name" ><button @click="onFetchPokemon">Fetch pokemon</button>
+  </div>
+  <div v-if="foundPokemon">
+    <div class="pokemon-detail">
+      <div class="thumbnail"><img src="foundPokemon.sprites.frontdefault" alt=""></div>
+      <div class="info">
+        <h3>{{foundPokemon.name}}</h3>
+        <p>Height: {{foundPokemon.height}} Weight: {{foundPokemon.weight}}</p>
+        <div class="move" v-for="move in foundPokemon.moves" :key="move.name">{{move.name}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import { PokemonClient } from 'pokenode-ts';
+import { PokemonClient, Pokemon  } from 'pokenode-ts';
 
 export default defineComponent({
   name: 'Home',
   components: {},
   setup() {
-    const title = ref('Welcome to pokeapi explorer')
+    const title = ref('Welcome to pokeapi explorer');
+    const name = ref('');
+    const foundPokemon = ref<Pokemon>()
 
     const onFetchPokemon = async() => {
       const api = new PokemonClient();
 
       await api
-      .getPokemonByName('luxray')
-      .then((data) => console.log(data)) // will output "Luxray"
+      .getPokemonByName(name.value)
+      .then((data: Pokemon) => {
+        foundPokemon.value = data;
+      })
       .catch((error) => console.error(error));
 
     }
 
-    return { title, onFetchPokemon }
+    return { title, onFetchPokemon, name, foundPokemon }
   }
 });
 </script>
