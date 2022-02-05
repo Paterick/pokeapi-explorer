dbc7310aa651c779d9018bf781e9452b28c8ab28
Patrick Winfield <patrwinfspam+github@gmail.com>
2022-02-05 10:26:45 -0700

    remove boilerplate code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <p>{{title}}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  components: {},
+  setup() {
+    const title = ref('Welcome to pokeapi explorer')
+
+    return { title }
+  }
 });
 </script>
